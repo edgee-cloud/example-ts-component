fb75f847015a9cdbd8db9a873d3d1494495b4920
Clement Bouvet <clement@cbouvet.fr>
2025-07-01 11:12:00 +0200

    ts: edge-function component

---
+++ edge-function/types/wit.d.ts
@@ -0,0 +1,8 @@
+// world edgee:native/edge-function
+export type * as WasiClocksMonotonicClock020 from './interfaces/wasi-clocks-monotonic-clock.js'; // import wasi:clocks/monotonic-clock@0.2.0
+export type * as WasiHttpOutgoingHandler020 from './interfaces/wasi-http-outgoing-handler.js'; // import wasi:http/outgoing-handler@0.2.0
+export type * as WasiHttpTypes020 from './interfaces/wasi-http-types.js'; // import wasi:http/types@0.2.0
+export type * as WasiIoError020 from './interfaces/wasi-io-error.js'; // import wasi:io/error@0.2.0
+export type * as WasiIoPoll020 from './interfaces/wasi-io-poll.js'; // import wasi:io/poll@0.2.0
+export type * as WasiIoStreams020 from './interfaces/wasi-io-streams.js'; // import wasi:io/streams@0.2.0
+export * as incomingHandler from './interfaces/wasi-http-incoming-handler.js'; // export wasi:http/incoming-handler@0.2.0
