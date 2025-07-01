7f2ff8d0c6a9f3345cb2036775a89199617ac7bd
Clement Bouvet <clement@cbouvet.fr>
2025-07-01 10:38:54 +0200

    remove consent management

--- consent-management/test/register-loader.js
+++
@@ -1,5 +0,0 @@
-/** This file is needed to load .ts files by Mocha (without experimental warnings) */
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-register('ts-node/esm', pathToFileURL('./'));
--- consent-management/types/wit.d.ts
+++
@@ -1,2 +0,0 @@
-// world edgee:native/consent-management
-export * as consentManagement from './interfaces/edgee-components-consent-management.js'; // export edgee:components/consent-management@1.0.0
